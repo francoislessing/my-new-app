918b1fb7ee3b45ec34f4ee650194b7fcf243288e
Francois Lessing <francois.lessing@gmail.com>
2019-02-28 11:19:32 +0200

    Update routing info for list-satellites

--- src/app/list-satellites/list-satellites-routing.module.ts
+++ src/app/list-satellites/list-satellites-routing.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthGuard } from '../auth/auth-guard.service';
+import { ListAllSatellitesComponent } from './list-all-satellites/list-all-satellites.component';
+import { ListSatellitesComponent } from './list-satellites.component';
+
 // import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 // import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 // import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 // import { RecipesComponent } from './recipes.component';
 
 const satellitesRoutes: Routes = [
+  {path : '', component : ListSatellitesComponent
+  , children: [
+    { path: '', component: ListAllSatellitesComponent }  
+  ]  
+  }
 //   { path: '', component: RecipesComponent, children: [
 //     { path: '', component: RecipeStartComponent },
 //     { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
