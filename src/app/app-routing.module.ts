6757f801de0e3a8a9699ec371d3866b956ee57b7
Francois Lessing <francois.lessing@gmail.com>
2019-02-28 11:13:33 +0200

    Updated routing for app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+// import { ListSatellitesComponent } from './list-satellites/list-satellites.component'; // static module import
 import { HomeComponent } from './core/home/home.component';
+
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'list-satellites', loadChildren : './list-satellites/list-satellites.module#ListSatellitesModule'},
+
   { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
   { path: 'shopping-list', component: ShoppingListComponent }
+  // ,{ path: 'list-satellites', component: ListSatellitesComponent }
 ];
 
 @NgModule({
