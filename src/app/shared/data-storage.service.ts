ff18762d995f5cf67f836c92fb063e775c207a2d
Francois Lessing <francois.lessing@gmail.com>
2019-03-03 18:37:32 +0200

    Update data storage service  with getSatellites method

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,12 +4,15 @@ import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { SatelliteReceived } from '../list-satellites/list-satellites.model'
 import { AuthService } from '../auth/auth.service';
+import { SatelliteService } from '../list-satellites/list-satellites.service';
 
 @Injectable()
 export class DataStorageService {
   constructor(private http: Http,
               private recipeService: RecipeService,
+              private satelliteService: SatelliteService,
               private authService: AuthService) {
   }
 
@@ -40,4 +43,27 @@ export class DataStorageService {
         }
       );
   }
+
+  getSatellites() {
+    
+
+    this.http.get('https://gnssplanningbeta.azurewebsites.net/api/SatelliteLocations/') 
+      .map(
+        (response: Response) => {
+          // Check what happens here
+          const satellites: SatelliteReceived[] = response.json();
+          console.log(satellites);
+          // for (let sat of satellites) {
+           
+          // }
+          return satellites;
+        }
+      )
+      .subscribe(
+        (satellites: SatelliteReceived[]) => {
+          this.satelliteService.setSatellites(satellites);
+        }
+      );
+  }
+
 }
